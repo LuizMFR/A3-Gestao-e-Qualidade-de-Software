75045da47f811f7727301205489a39381aa41c33
unknown <leandrorsmlp@hotmail.com>
2025-10-31 22:17:57 -0300

    Adição de comentário para entender as camadas de Service, Repository e Controler

--- Servidor/src/main/java/com/example/Servidor/repositories/UserRepository.java
+++ Servidor/src/main/java/com/example/Servidor/repositories/UserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.Servidor.entities.User;
 
+/*REPOSITÓRIO É ONDE ESTÁ OS COMANDOS SQL, NESTE CASO, JÁ ESTOU IMPLEMENTADOS PELO JPAREPOSITORY */
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer>{
 

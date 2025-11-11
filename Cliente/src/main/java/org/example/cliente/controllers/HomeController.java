a574b4ec0fa86980b0d0d75b4a679077f61b14f0
Michel Ferreira <michel.mf628@gmail.com>
2025-11-11 20:19:07 -0300

    commit para arrumar o home.fzml

--- Cliente/src/main/java/org/example/cliente/controllers/HomeController.java
+++ Cliente/src/main/java/org/example/cliente/controllers/HomeController.java
@@ -51,9 +51,7 @@ public class HomeController {
     // Método chamado automaticamente após carregar o FXML
     @FXML
     public void initialize() {
-        configurarTabela();
-        carregarTransacoes();
-        atualizarResumo();
+        
     }
     public void setStageSize(double width, double height) {
         Stage stage = (Stage) lblSaldo.getScene().getWindow();
